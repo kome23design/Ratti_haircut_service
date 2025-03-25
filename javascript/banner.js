fd83bcebe71e2eb5d65df512ed42e34953f913a3
kome23design <komemartin1@gmail.com>
2025-03-25 08:32:04 +0100

    readjust folder

---
+++ javascript/banner.js
@@ -0,0 +1,19 @@
+const banner=async()=>{
+    const data = await fetch("./data2.json",{
+        method:'GET',
+        headers:{'content-Type':'application/json'}
+    })
+    .then((res)=>res.json())
+    return data
+}
+let image=document.querySelector(".img_class")
+let e=0
+setInterval(async function(){
+    e++
+    const datafxn = await banner()
+    if (e>=datafxn.length){
+        e=0
+    }
+    // console.log(datafxn[e].image)
+    image.src="./images/"+datafxn[e].image
+},3000)
